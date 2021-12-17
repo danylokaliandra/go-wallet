4788f9a0d7cc4ddfc8f828dccde38a9fa5d71d27
danylokaliandra <danylokaliandra@gmail.com>
2021-12-17 14:01:19 +0100

    Añado fichero test moneyAccount
    Añado framework para excepciones. Closes #24

---
+++ internal/mywallet/moneyAccount_test.go
@@ -0,0 +1,16 @@
+package mywallet
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSomething(t *testing.T) {
+
+	var a string = "Hello"
+	var b string = "Hello"
+
+	assert.Equal(t, a, b, "The two words should be the same.")
+
+}
